44e325e5875e899a047b093a404dcef9b35c441c
COMOJA66 <comoja66@gmail.com>
2026-03-12 11:07:20 -0600

    Aplicar filtro de extensiones y excluir archivos ocultos

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def saludo():
-    print("🐍 ¡Hola desde Python en Mac!")
-    print(f"Versión: {sys.version.split()[0]}")
-
-if __name__ == "__main__":
-    saludo()
--- frontend/test.java
+++
@@ -1,6 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("☕️ ¡Hola desde Java en Mac!");
-        System.out.println("Arquitectura: " + System.getProperty("os.arch"));
-    }
-}
